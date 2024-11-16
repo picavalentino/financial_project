024a6d1140758b7b0bdc195e7b3ce1d0a1870cf3
KSB <picachu84@naver.com>
2024-11-16 17:22:20 +0900

    초기 파일입니다.

---
+++ src/main/java/com/team/financial_project/FinancialProjectApplication.java
@@ -0,0 +1,13 @@
+package com.team.financial_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialProjectApplication.class, args);
+	}
+
+}
