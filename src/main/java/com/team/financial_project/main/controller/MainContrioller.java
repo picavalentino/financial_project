3f9b222e0b49dd9903bc90ba77b791806572c40d
KSB <pica84@nate.com>
2024-11-20 12:37:15 +0900

    배포테스트를 위한 컨트롤러 변경2

--- src/main/java/com/team/financial_project/main/controller/MainContrioller.java
+++ src/main/java/com/team/financial_project/main/controller/MainContrioller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainContrioller {
     // test용
-    @GetMapping("/test")
+    @GetMapping("/")
     public String test() {
         return "financial/test";
     }
