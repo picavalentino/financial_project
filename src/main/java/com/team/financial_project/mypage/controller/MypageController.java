b31377e8c38bba79fe33e9b7fa5e18c1dc77d6c5
yum05 <hase001224@gmail.com>
2024-11-22 18:18:39 +0900

    마이페이지 데이터 불러오기 성공

--- src/main/java/com/team/financial_project/mypage/controller/MypageController.java
+++ src/main/java/com/team/financial_project/mypage/controller/MypageController.java
@@ -1,12 +1,36 @@
 package com.team.financial_project.mypage.controller;
 
+import com.team.financial_project.mypage.dto.MypageDTO;
+import com.team.financial_project.mypage.service.MypageService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MypageController{
-    @GetMapping("mypage")
-    public String mypage(){
-        return "mypage/mypage";
+
+    private final MypageService mypageService;
+
+    public MypageController(MypageService mypageService) {
+        this.mypageService = mypageService;
     }
+
+    @GetMapping("mypage/{userId}")
+    public ResponseEntity<MypageDTO> getMypage(@PathVariable String userId) {
+        return ResponseEntity.ok(mypageService.getMypageByUserId(userId));
+    }
+    @GetMapping("/mypage")
+    public String mypage(Model model, @RequestParam("userId") String userId) {
+        MypageDTO mypageDTO = mypageService.getMypageByUserId(userId);
+        model.addAttribute("mypage", mypageDTO);
+        return "mypage/mypage"; // Thymeleaf 파일
+    }
+
+
+
+
 }
--- src/main/java/com/team/financial_project/mypage/service/MypageService.java
+++ src/main/java/com/team/financial_project/mypage/service/MypageService.java
@@ -1,4 +1,23 @@
 package com.team.financial_project.mypage.service;
 
+import com.team.financial_project.mypage.dto.MypageDTO;
+import com.team.financial_project.mypage.mapper.MypageMapper;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MypageService {
+
+    private final MypageMapper mypageMapper;
+
+    public MypageService(MypageMapper mypageMapper) {
+        this.mypageMapper = mypageMapper;
+    }
+
+    public MypageDTO getMypageByUserId(String userId) {
+        MypageDTO mypageDTO = mypageMapper.selectUserInfo(userId); // 유저 정보 가져오기
+        mypageDTO.setInquiries(mypageMapper.selectInquiriesByUserId(userId)); // 상담 리스트
+        mypageDTO.setCustProds(mypageMapper.selectCustProdsByUserId(userId)); // 가입 상품 리스트
+        return mypageDTO;
+    }
+
 }
