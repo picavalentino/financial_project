f65bee3ff5770d43cbbb44eadd4e83b293a99bc7
hyell <hyelee9295@gamil.com>
2024-11-27 11:36:53 +0900

    직원관리 완료

--- src/main/resources/static/js/management/managementInsert.js
+++ src/main/resources/static/js/management/managementInsert.js
@@ -31,80 +31,6 @@ document.getElementById('btn-reset').addEventListener('click', function() {
       window.location.href = '/management/insert';
    });
 
-$(document).ready(function () {
-    $("#btn-register").on("click", function () {
-
-        // Bootstrap 5 Modal을 JavaScript로 열기
-        var insertModal = new bootstrap.Modal(document.getElementById('InsertModal'));
-        insertModal.show();
-
-        // 서버로부터 데이터 가져오기
-        $.ajax({
-            type: 'GET',
-            url: '/management/employee/insertModal',
-            success: function (response) {
-
-                // 부서 정보 채우기
-                let departmentSelect = $('#department');
-                departmentSelect.empty();
-                departmentSelect.append('<option value="" selected>부서</option>');
-                response.departmentList.forEach(function (department) {
-                    departmentSelect.append('<option value="' + department.user_dept_cd + '">' + department.dept_name + '</option>');
-                });
-
-                // 직위 정보 채우기
-                let positionSelect = $('#position');
-                positionSelect.empty();
-                positionSelect.append('<option value="" selected>직위</option>');
-                response.jobPositionList.forEach(function (position) {
-                    positionSelect.append('<option value="' + position.user_jbps_ty_cd + '">' + position.position_name + '</option>');
-                });
-
-                // 권한 정보 채우기
-                let authSelect = $('#auth');
-                authSelect.empty();
-                authSelect.append('<option value="" selected>권한</option>');
-                response.authList.forEach(function (auth) {
-                    authSelect.append('<option value="' + auth.user_auth_cd + '">' + auth.auth_name + '</option>');
-                });
-
-                // 상태 정보 채우기
-                let statusSelect = $('#status');
-                statusSelect.empty();
-                statusSelect.append('<option value="" selected>상태</option>');
-                response.statusList.forEach(function (status) {
-                    statusSelect.append('<option value="' + status.user_status + '">' + status.status_name + '</option>');
-                });
-            },
-            error: function () {
-                 console.error('Error fetching employee data:', error);
-            }
-        });
-    });
-
-    $(".btn-userIdGenerate").on("click", function() {
-            let joiningDate = $("#user_jncmp_ymd").val(); // 입사일자 값 가져오기
-
-            if (!joiningDate) {
-                alert("입사일자를 먼저 입력해주세요.");
-                return;
-            }
-
-            // 서버로부터 사원번호 생성 요청
-            $.ajax({
-                type: "POST",
-                url: "/management/generate/userId",
-                data: { joiningDate: joiningDate },
-                success: function(user_id) {
-                    $("#user_id").val(user_id); // 생성된 사원번호를 입력 필드에 채움
-                },
-                error: function() {
-                    alert("사원번호를 생성하는데 실패했습니다.");
-                }
-            });
-        });
-});
-
 document.getElementById('user_birthday').addEventListener('input', function (e) {
     const input = e.target.value;
     const regex = /^\d{4}-\d{2}-\d{2}$/;
@@ -143,24 +69,92 @@ document.getElementById('user_birthday').addEventListener('input', function (e) 
 });
 
 $(document).ready(function () {
-$(".btn-submit").on("click", function (e) {
+    $("#btn-register").on("click", function () {
 
-    const userIdField = document.getElementById('user_id');
+        // 서버로부터 데이터 가져오기
+        $.ajax({
+            type: 'GET',
+            url: '/management/employee/insertModal',
+            success: function (response) {
+                // 부서 정보 채우기
+                let departmentSelect = $('#department');
+                departmentSelect.empty();
+                departmentSelect.append('<option value="" selected>부서</option>');
+                response.departmentList.forEach(function (department) {
+                    departmentSelect.append('<option value="' + department.user_dept_cd + '">' + department.dept_name + '</option>');
+                });
 
-    if (!userIdField.value) {
-        e.preventDefault(); // 폼 제출 방지
-        alert("사원번호를 생성해주세요."); // 경고 메시지
-        return;
+                // 직위 정보 채우기
+                let positionSelect = $('#position');
+                positionSelect.empty();
+                positionSelect.append('<option value="" selected>직위</option>');
+                response.jobPositionList.forEach(function (position) {
+                    positionSelect.append('<option value="' + position.user_jbps_ty_cd + '">' + position.position_name + '</option>');
+                });
+
+                // 권한 정보 채우기
+                let authSelect = $('#auth');
+                authSelect.empty();
+                authSelect.append('<option value="" selected>권한</option>');
+                response.authList.forEach(function (auth) {
+                    authSelect.append('<option value="' + auth.user_auth_cd + '">' + auth.auth_name + '</option>');
+                });
+
+                // 상태 정보 채우기
+                let statusSelect = $('#status');
+                statusSelect.empty();
+                statusSelect.append('<option value="" selected>상태</option>');
+                response.statusList.forEach(function (status) {
+                    statusSelect.append('<option value="' + status.user_status + '">' + status.status_name + '</option>');
+                });
+            },
+            error: function (xhr, status, error) {
+                 console.error('Error fetching employee data:', error);
+            }
+        });
+    });
+
+    $(".btn-userIdGenerate").on("click", function() {
+        let joiningDate = $("#user_jncmp_ymd").val(); // 입사일자 값 가져오기
+
+        if (!joiningDate) {
+            alert("입사일자를 먼저 입력해주세요.");
+            return;
+        }
+
+        // 서버로부터 사원번호 생성 요청
+        $.ajax({
+            type: "POST",
+            url: "/management/generate/userId",
+            data: { joiningDate: joiningDate },
+            success: function(user_id) {
+                $("#user_id").val(user_id); // 생성된 사원번호를 입력 필드에 채움
+            },
+            error: function() {
+                alert("사원번호를 생성하는데 실패했습니다.");
+            }
+        });
+    });
+
+    $(".btn-submit").on("click", function (e) {
+        const userIdField = document.getElementById('user_id');
+
+        if (!userIdField.value) {
+            e.preventDefault(); // 폼 제출 방지
+            alert("사원번호를 생성해주세요."); // 경고 메시지
+            return;
         }
     });
 
-     $(".btn-close").on("click", function () {
-            var insertModal = bootstrap.Modal.getInstance(document.getElementById('InsertModal'));
-            if (insertModal) {
-                insertModal.hide(); // 모달 닫기
-            }
+    $(".btn-close").on("click", function () {
+        // 모달이 닫힌 후 페이지를 리디렉션
+        window.location.href = '/management/insert';
+    });
+});
 
-            // 모달이 닫힌 후 페이지를 리디렉션
-            window.location.href = '/management/insert';
-        });
+//// 모든 readonly 요소에 대해 선택 방지
+document.querySelectorAll('input[readonly]').forEach(input => {
+    input.addEventListener('focus', (e) => e.target.blur()); // 포커스 제거
+    input.addEventListener('mousedown', (e) => e.preventDefault()); // 클릭 방지
+    input.addEventListener('selectstart', (e) => e.preventDefault()); // 텍스트 선택 방지
 });
