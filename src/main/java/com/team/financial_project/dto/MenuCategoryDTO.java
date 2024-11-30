f77c5e828d76d61314541ee502051dbcc31ce50d
sun <seon04520@gmail.com>
2024-11-30 17:25:46 +0900

    사이드바에 들어갈 데이터 가공 완료

--- src/main/java/com/team/financial_project/dto/MenuCategoryDTO.java
+++ src/main/java/com/team/financial_project/dto/MenuCategoryDTO.java
@@ -1,12 +1,14 @@
 package com.team.financial_project.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 public class MenuCategoryDTO {
     private String iconImg;
     private String categoryName;
-    private List<MenuDTO> menuList;
+    private List<SubMenuDTO> menuList;
 }
--- src/main/java/com/team/financial_project/main/service/SidebarService.java
+++ src/main/java/com/team/financial_project/main/service/SidebarService.java
@@ -2,12 +2,13 @@ package com.team.financial_project.main.service;
 
 import com.team.financial_project.dto.AuthSystemDTO;
 import com.team.financial_project.dto.MenuCategoryDTO;
-import com.team.financial_project.dto.MenuDTO;
+import com.team.financial_project.dto.SubMenuDTO;
 import com.team.financial_project.mapper.AuthSystemMapper;
 import com.team.financial_project.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -36,9 +37,54 @@ public class SidebarService {
 
     public List<MenuCategoryDTO> getMenuList() {
         List<AuthSystemDTO> authMenu = authSystemMapper.getMenuList();
-        authMenu.forEach(m-> {
-            
-        });
+        List<MenuCategoryDTO> menuCategoryList = new ArrayList<>();
+        String commonUrl = "/images/common/";
+        String imgType = ".png";
+
+        for(AuthSystemDTO authData : authMenu){ // 가져온 정보 루프
+            for (MenuCategoryDTO menuCategory : menuCategoryList){ // 가공된 카테고리 루프
+                if(menuCategory.getCategoryName().equals(authData.getMenu_category())){ // 해당 카테고리가 이미 존재한다면
+                    List<SubMenuDTO> subMenuList = menuCategory.getMenuList();
+
+                    for(SubMenuDTO subMenu : subMenuList){
+                        if(subMenu.getName().equals(authData.getMenu_name())){ // 권한 리스트에 같은 이름의 서브메뉴가 있다면
+                            // 한 줄로 가공 ex) "MASTER HR_TEAM"
+                            subMenu.setHasAnyRole(subMenu.getHasAnyRole() + " " + authData.getAuth_name());
+                        }else { // 없으면 새로운 서브메뉴 생성 후 넣기
+                            // 새로운 서브메뉴 생성
+                            SubMenuDTO newSubMenu = new SubMenuDTO(
+                                    authData.getMenu_id(),
+                                    authData.getMenu_url(),
+                                    authData.getMenu_name(),
+                                    authData.getAuth_name()
+                            );
+                            // 해당 카테고리 서브메뉴 리스트에 추가 후 카테고리 업데이트
+                            subMenuList.add(newSubMenu);
+                        }
+                    }
+                }else {
+                    // 새로운 서브메뉴 생성
+                    SubMenuDTO newSubMenu = new SubMenuDTO(
+                            authData.getMenu_id(),
+                            authData.getMenu_url(),
+                            authData.getMenu_name(),
+                            authData.getAuth_name()
+                    );
+                    // 새로운 서브메뉴 리스트에 넣어주기
+                    List<SubMenuDTO> subMenuList = new ArrayList<>();
+                    subMenuList.add(newSubMenu);
+                    // 새로운 메뉴카테고리 생성
+                    MenuCategoryDTO newMenuCategory = new MenuCategoryDTO(
+                            commonUrl + authData.getMenu_category().trim() +imgType,
+                            authData.getMenu_category(),
+                            subMenuList
+                    );
+                    // 메뉴 카테고리 리스트에 넣어주기
+                    menuCategoryList.add(newMenuCategory);
+                }
+            }
+        }
+        menuCategoryList.forEach(System.out::println);
         return null;
     }
 }
