98d35105aad334afc814472d8d4c6f40f69a51ec
yeeeone <gim-yewon@gim-yewon-ui-MacBookAir.local>
2024-11-30 09:54:29 +0900

    권한관리 페이지 오류 수정

--- src/main/java/com/team/financial_project/authSystem/controller/AuthSystemController.java
+++ src/main/java/com/team/financial_project/authSystem/controller/AuthSystemController.java
@@ -60,6 +60,6 @@ public class AuthSystemController {
         // 검색 조건 모델에 추가
         model.addAttribute("auth", auth);
 
-        return "/authSystem/authSystem";
+        return "authSystem/authSystem";
     }
 }
