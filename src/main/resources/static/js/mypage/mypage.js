5808dbc3b45d1f0d5675948df781a994fd66c434
yum05 <hase001224@gmail.com>
2024-12-01 22:15:05 +0900

    전화 인증(중복 채크) 추가

--- src/main/resources/static/js/mypage/mypage.js
+++ src/main/resources/static/js/mypage/mypage.js
@@ -27,6 +27,32 @@ $(document).ready(function () {
             $eyeClose.hide(); // 닫힘 아이콘 숨기기
         }
     });
+$("#phone_btn").on("click", function() {
+    // 전화번호 조합
+    const phonePart1 = $("#phone_input1").val();
+    const phonePart2 = $("#phone_input2").val();
+    const phonePart3 = $("#phone_input3").val();
+    const phoneNumber = phonePart1 + "-" + phonePart2 + "-" + phonePart3; // 전화번호 조합
+
+    // 서버로 AJAX 요청
+    $.ajax({
+        url: "/mypage/certify",
+        type: "GET",
+        data: { telno: phoneNumber },
+        success: function (response) {
+            if (response) {
+                $("#phone_btn").prop("disabled", true).val("인증완료");
+                alert("인증되었습니다.");
+            } else {
+                alert("이미 등록된 번호입니다.");
+            }
+        },
+        error: function (xhr, status, error) {
+            console.error("Error:", error);
+            alert("인증 요청 중 오류가 발생했습니다. 다시 시도해주세요.");
+        }
+    });
+});
 
 
     // CSRF 토큰 및 헤더 가져오기
